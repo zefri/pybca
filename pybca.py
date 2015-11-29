2c4daeab10fd2675bb3cb434b2db081ec0fcc9a7
Zefri Yazid <zefri_yazid@yahoo.com>
2015-11-29 04:04:33 +0100

    add save trans histories and tidy up data folder

--- pybca.py
+++ pybca.py
@@ -6,7 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.common.exceptions import *
+from selenium.webdriver.support import expected_conditions as EC
 
 from bs4 import BeautifulSoup
 
@@ -16,10 +18,11 @@ Base = declarative_base()
 class DbaseManager:
     
     def __init__(self, user):
-        if not os.path.exists("data"):
-            os.makedirs("data")
+        wkdir = "data/{0}".format(user) 
+        if not os.path.exists(wkdir):
+            os.makedirs(wkdir)
             
-        self.dbasePath = "data/{0}.db3".format(user)
+        self.dbasePath = "{0}/data.db3".format(wkdir)
         
     def create_table(self):
         
@@ -51,6 +54,12 @@ class DbaseManager:
             session.add(trans)
             session.commit()
         return trans
+    
+    def get_last_trans(self):
+        session = self.session()
+        return session.query(Trans, func.max(Trans.tanggal)).first()[1]
+        
+        
 
 class Trans(Base) :
     __tablename__ = 'trans'
@@ -77,50 +86,166 @@ class Trans(Base) :
 
 class AutoBrowser():
     
-    def grep(self, user, pwd):
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        self.dir = "data/{0}".format(user) 
         
-        driver = webdriver.Firefox()
+    def stop(self):
+        self.driver.quit();
+        
+    def save_all_evidences(self, user, pwd, fromDate):
+        
+        MONTHS = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli",
+        "Agustus", "September", "Oktober", "Nopember", "Desember"]
+        
         #login
-        driver.get("https://ibank.klikbca.com/")
-        driver.find_element_by_id("user_id").send_keys(user)
-        driver.find_element_by_id("pswd").send_keys(pwd)
-        driver.find_element_by_name("value(Submit)").click()
+        self.driver.get("https://ibank.klikbca.com/")
+        wait = WebDriverWait(self.driver,10)
+        
+        wait.until(lambda d: d.find_element_by_id("user_id"))
+        
+        self.driver.find_element_by_id("user_id").send_keys(user)
+        self.driver.find_element_by_id("pswd").send_keys(pwd)
+        self.driver.find_element_by_name("value(Submit)").click()
         # click menu
-        driver.switch_to.frame(driver.find_element_by_name("menu"))
-        driver.find_element_by_link_text('Informasi Rekening').click()
-        driver.find_element_by_link_text('Mutasi Rekening').click()
+        self.driver.switch_to.frame(self.driver.find_element_by_name("menu"))
+        self.driver.find_element_by_link_text('Histori Transaksi').click()
+        
         #pick date
-        driver.switch_to_default_content()
-        driver.switch_to.frame(driver.find_element_by_name("atm"))
-        select = Select(driver.find_element_by_name("value(startDt)"))
-        select.select_by_visible_text("01")
+        self.driver.switch_to_default_content()
+        self.driver.switch_to.frame(self.driver.find_element_by_name("atm"))
+        self.driver.find_element_by_css_selector("input[type='radio'][value='1']").click()
+        select = Select(self.driver.find_element_by_name("value(startDt)"))
+        select.select_by_visible_text(fromDate.strftime("%d"))
+        
+        select = Select(self.driver.find_element_by_name("value(startMt)"))
+        select.select_by_visible_text(MONTHS[fromDate.month-1])
+        
+        select = Select(self.driver.find_element_by_name("value(endDt)"))
+        select.select_by_visible_text(datetime.datetime.now().strftime("%d"))
+        
+        # to prevent Tanggal akhir lebih besar dari tanggal hari ini alert
+        try:
+            self.driver.find_element_by_name("value(submit)").send_keys(Keys.NULL)
+            self.driver.find_element_by_name("value(submit)").click()
+        
+            WebDriverWait(self.driver, 3).until(EC.alert_is_present(),
+                                           'Timed out waiting for Tanggal akhir lebih besar dari tanggal hari ini.')
 
-        select = Select(driver.find_element_by_name("value(endDt)"))
-        select.select_by_visible_text("28")
+            alert = self.driver.switch_to_alert()
+            alert.accept()
+            select.select_by_visible_text((datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%d"))
+            self.driver.find_element_by_name("value(submit)").click()
+            
+        except TimeoutException:
+            pass
+            
+        links = self.driver.find_elements_by_css_selector("a")
+        num = len(links)
+        for x in range(0, num):
+            links = self.driver.find_elements_by_css_selector("a")
+            links[x].click()
+            
+            fname = self.__get_fname(self.driver.page_source)
+            
+            self.driver.save_screenshot("{0}/{1}.jpg".format(self.dir, fname))
+            self.driver.find_element_by_css_selector("input[type='submit'][value='Kembali']").click()
+            wait.until(lambda d: len(d.find_elements_by_css_selector("a")) == num)
         
-        driver.find_element_by_name("value(submit1)").click()
+        self.driver.switch_to_default_content()
+        self.driver.switch_to.frame(self.driver.find_element_by_name("header"))
+        self.driver.find_element_by_link_text('[ LOGOUT ]').click()
+    
+    def __get_fname(self, html):
+        soup = BeautifulSoup(html, 'html.parser')
+        fonts = soup.find_all("font", attrs={"color":"#0000bb"})
+        tgl = fonts[4].string
+        jam = fonts[7].string
+        nama = fonts[16].string
+        if len(fonts[19].find_all("font")) == 0:
+            return "=".join([tgl, jam, nama])
         
-        html = driver.page_source
+        jml = fonts[19].find_all("font")[1].string
         
-        driver.switch_to_default_content()
-        driver.switch_to.frame(driver.find_element_by_name("header"))
-        driver.find_element_by_link_text('[ LOGOUT ]').click()
-        driver.quit();
+        return "=".join([tgl, jam, nama, jml])
+        
+    def grep(self, user, pwd, fromDate):
+        
+        MONTHS = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli",
+        "Agustus", "September", "Oktober", "Nopember", "Desember"]
+        
+        #login
+        self.driver.get("https://ibank.klikbca.com/")
+        
+        #wait
+        wait = WebDriverWait(self.driver,10)
+        wait.until(lambda d: d.find_element_by_id("user_id"))
+        
+        self.driver.find_element_by_id("user_id").send_keys(user)
+        self.driver.find_element_by_id("pswd").send_keys(pwd)
+        self.driver.find_element_by_name("value(Submit)").click()
+        # click menu
+        self.driver.switch_to.frame(self.driver.find_element_by_name("menu"))
+        self.driver.find_element_by_link_text('Informasi Rekening').click()
+        self.driver.find_element_by_link_text('Mutasi Rekening').click()
+        #pick date
+        self.driver.switch_to_default_content()
+        self.driver.switch_to.frame(self.driver.find_element_by_name("atm"))
+        select = Select(self.driver.find_element_by_name("value(startDt)"))
+        select.select_by_visible_text(fromDate.strftime("%d"))
+        
+        select = Select(self.driver.find_element_by_name("value(startMt)"))
+        select.select_by_visible_text(MONTHS[fromDate.month-1])
+        
+        select = Select(self.driver.find_element_by_name("value(endDt)"))
+        select.select_by_visible_text(datetime.datetime.now().strftime("%d"))
+        
+        # to prevent Tanggal akhir lebih besar dari tanggal hari ini alert
+        try:
+            self.driver.find_element_by_name("value(submit1)").send_keys(Keys.NULL)
+            self.driver.find_element_by_name("value(submit1)").click()
+        
+            WebDriverWait(self.driver, 3).until(EC.alert_is_present(),
+                                           'Timed out waiting for Tanggal akhir lebih besar dari tanggal hari ini.')
+
+            alert = self.driver.switch_to_alert()
+            alert.accept()
+            select.select_by_visible_text((datetime.datetime.now()-datetime.timedelta(days=1)).strftime("%d"))
+            self.driver.find_element_by_name("value(submit1)").click()
+        except TimeoutException:
+            pass
+            # no alert
+                
+        html = self.driver.page_source
+        
+        self.driver.switch_to_default_content()
+        self.driver.switch_to.frame(self.driver.find_element_by_name("header"))
+        self.driver.find_element_by_link_text('[ LOGOUT ]').click()
         
         # parse content
         return self.__parse(html)
+    
+    def __is_alert_present(self, driver):
+        try:
+            alert = driver.switch_to_alert()
+            alert.accept()
+            return True
+        except NoAlertPresentException as ex:
+            return False
     
     def __parse(self, html):
         soup = BeautifulSoup(html, 'html.parser')
         tables = soup.find_all("table")
         trs = tables[4].find_all("tr")
-        for x in range(1, len(trs)-1):
-            tgl = trs[x].find_all("td")[0].text.strip()
-            ket = trs[x].find_all("td")[1].text.strip()
-            cab = trs[x].find_all("td")[2].text.strip()
-            jml = float(trs[1].find_all("td")[3].text.strip().replace(",", ""))
-            tipe = trs[x].find_all("td")[4].text.strip()
-            saldo = float(trs[1].find_all("td")[5].text.strip().replace(",", ""))
+        for x in range(1, len(trs)):
+            tds = trs[x].find_all("td")
+            
+            tgl = tds[0].text.strip()
+            ket = tds[1].text.strip()
+            cab = tds[2].text.strip()
+            jml = float(tds[3].text.strip().replace(",", ""))
+            tipe = tds[4].text.strip()
+            saldo = float(tds[5].text.strip().replace(",", ""))
             
             if len(tgl.split("/")) == 2 :
                 day, mon = tgl.split("/")
@@ -136,10 +261,19 @@ class PyBCA :
         self.dbman = DbaseManager(user)
         self.dbman.create_table()
         
-    def save(self, fromDate):
-        for tran in AutoBrowser().grep(self.user, self.pwd):
+    def save(self):
+        last = self.dbman.get_last_trans()
+        print(last)
+        if last == None:
+            last = datetime.datetime.now()-datetime.timedelta(days=31)
+        
+        browser = AutoBrowser()
+        for tran in browser.grep(self.user, self.pwd, last):
             self.dbman.save(tran)
             print(tran)
+            
+        browser.save_all_evidences(self.user, self.pwd, last)
+        browser.stop()
         
 if __name__ == "__main__":    
     
@@ -149,5 +283,6 @@ if __name__ == "__main__":
         
     _, user, name = sys.argv
     bca = PyBCA(user, name)
-    bca.save(datetime.datetime.now())
+    bca.save()
+    
     
